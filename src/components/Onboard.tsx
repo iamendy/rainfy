4e23c16e05f3f8212011fa575cc00804d563438e
Nnamdi <17215830+iamendy@users.noreply.github.com>
2023-09-11 17:15:39 +0100

    update: Added piggybank UI

--- src/components/Onboard.tsx
+++ src/components/Onboard.tsx
@@ -2,16 +2,17 @@ import Rainy from "../components/Rainy";
 
 const Onboard = () => {
   return (
-    <div className="p-6 h-full ">
-      <div className="flex items-center justify-center flex-col">
-        <Rainy />
+    <section className="flex items-center flex-col h-full">
+      <Rainy />
+
+      <div className="text-center">
         <h3>Save for the rainy day</h3>
         <div>Please connect your Monerium account</div>
         <button className="bg-yellow font-bold text-base-100 px-4 py-2 rounded-lg mt-4">
           Connect Monerium
         </button>
       </div>
-    </div>
+    </section>
   );
 };
 export default Onboard;
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,14 @@
 import Onboard from "../components/Onboard";
+import PiggieBank from "../components/PiggieBank";
+
+import { useState } from "react";
 
 const Dashboard = () => {
-  return <Onboard />;
+  const [isOnboard, setIsOnboard] = useState(false);
+
+  return (
+    <div className="p-6 h-full">{isOnboard ? <Onboard /> : <PiggieBank />}</div>
+  );
 };
 
 export default Dashboard;
